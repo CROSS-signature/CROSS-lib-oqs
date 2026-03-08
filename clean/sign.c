179d61b8b59e524fba86954f7fa269495650030a
rtjk <47841774+rtjk@users.noreply.github.com>
2026-03-08 19:49:49 +0100

    add a check on the minimum smlen

--- clean/sign.c
+++ clean/sign.c
@@ -83,6 +83,10 @@ int crypto_sign_open(unsigned char *m,                          // out parameter
                      const unsigned char *pk)                   // in parameter
 {
 
+   if(smlen < (size_t) sizeof(CROSS_sig_t)) {
+      return -1;
+   }
+
    /* verify returns 1 if signature is ok, 0 otherwise */
    *mlen = smlen-(size_t) sizeof(CROSS_sig_t);
    
